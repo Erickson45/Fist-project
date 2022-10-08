282c1e48bdd0fa002c1f1e2d8e32ab7bd89dcd28
Erickson45 <erickson.queiroz65@gmail.com>
2022-10-08 12:40:14 -0300

    primeiro projeto

---
+++ Pasta do curso/comentario.js
@@ -0,0 +1,9 @@
+// comentario para não sair no código e fazer uma anotação
+console.log('passo #1');
+console.log('passo #2'); // mais um exemplo de comentário
+// console.log('passo #3');
+
+/*
+Comentario
+irineu
+*/ 
